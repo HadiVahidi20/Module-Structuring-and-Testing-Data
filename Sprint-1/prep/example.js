c346f5228ba19fe20556d0a8df57a0974247a358
HadiVahidi20 <hadivahidi20@gmail.com>
2025-09-30 12:20:06 +0100

    percentage exersice added

--- Sprint-1/prep/example.js
+++ Sprint-1/prep/example.js
@@ -1,21 +1,7 @@
-// Your birthdate
-const birthYear = 1992;
-const birthMonth = 3; // March (1 = January, 2 = February, 3 = March, etc.)
-const birthDay = 21;  // replace with your actual day of birth
+//percentage exercise
 
-// Current date
-const today = new Date();
-const currentYear = today.getFullYear();
-const currentMonth = today.getMonth() + 1; // getMonth() is 0-based
-const currentDay = today.getDate();
+const decimalNumber = 0.5;
 
-// Calculate age
-let age = currentYear - birthYear;
+const percentage = `${decimalNumber * 100} %`
 
-// Adjust if birthday hasn't happened yet this year
-if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
-  age--;
-}
-
-console.log(`You are ${age} years old.`);
-
+console.log(percentage);
