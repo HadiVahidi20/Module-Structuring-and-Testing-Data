ef4f64230beafbce791d9fa29b1fb5744d0f9bc5
HadiVahidi20 <hadivahidi20@gmail.com>
2025-10-01 17:17:57 +0100

    Add comments to clarify count variable increment logic

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+
+
+//line 3 is updating the value of count by adding 1 to its current value, so count now equals 1 and every time we run the code it will increment by 1.
