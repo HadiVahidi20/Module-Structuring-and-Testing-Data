24ee9040c96502b297189d392efbc0ce4f8d6cbe
HadiVahidi20 <hadivahidi20@gmail.com>
2025-10-02 17:18:37 +0100

    Fix variable initialization error by moving console.log after declaration in 2.js

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,17 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+//console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+/*/home/cyf/CYF/ITP/Module-Structuring-and-Testing-Data/Sprint-1/2-mandatory-errors/2.js:4
+console.log(`I was born in ${cityOfBirth}`);
+                             ^
+
+ReferenceError: Cannot access 'cityOfBirth' before initialization
+
+this error is because we are trying to access the variable cityOfBirth before it is initialized and assigned a value.
+ the console.log should be after the variable declaration and assignment
+
+*/
+console.log(`I was born in ${cityOfBirth}`);
